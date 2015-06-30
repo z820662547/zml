4db05478b3de09332d99c81b5b7a8ae5a97f3d51
z820662547 <zml_vector@163.com>
2015-06-30 18:05:15 +0800

    2.0

--- src/com/example/linkifytest/MainActivity.java
+++ src/com/example/linkifytest/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.linkifytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnKeyListener;
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState); 
 		setContentView(R.layout.activity_main);   
+		final EditText editText=(EditText)findViewById(R.id.EditText01);
+		final TextView tView=(TextView)findViewById(R.id.TextView01);
+		editText.setOnKeyListener(new OnKeyListener() {
+			
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// TODO Auto-generated method stub
+				tView.setText(editText.getText());
+				Linkify.addLinks(tView, Linkify.WEB_URLS|Linkify.EMAIL_ADDRESSES|Linkify.PHONE_NUMBERS);
+				return false;
+			}
+		});
 	}
 
 }
